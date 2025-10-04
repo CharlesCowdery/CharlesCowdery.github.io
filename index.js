2ad09cb0dbc3722c6387d1784d50b2d51a17ce83
Charles Cowdery <charlie.r.cowdery@gmail.com>
2025-10-04 15:16:51 -0400

    added animation loop

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop( animate );
+
 document.body.appendChild(renderer.domElement);
 
 const geometries = new Map();
